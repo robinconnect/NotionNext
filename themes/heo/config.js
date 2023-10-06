a641c27fd751c21ce0104d2718d4e63105f47b2a
RobinConnect <129139820+robinconnect@users.noreply.github.com>
2023-10-06 12:03:20 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -6,7 +6,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
     { title: '', url: '' },
-    { title: '联合大湾区所有做国际业务的打工人', url: 'http://robinconnect.com' }
+    { title: '联合大湾区所有做国际业务的小伙伴', url: 'http://robinconnect.com' }
   ],
 
   // 英雄区(首页顶部大卡)
